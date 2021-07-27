5177ecb783c0d2f56574a6f63053e75f2118bc5e
Alchamyst <ashley@ashley-yetman.co.uk>
2021-07-27 19:01:10 +0100

    refactor header component to update favicon and include fallbacks.

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import faviconImg from "../images/icon.png"
+import favicon from "../images/favicon.ico"
+import faviSvg from "../images/logo.svg"
 
 const Header = (metaKeywords) => {
   return (
     <div>
         <Helmet>
             <meta name="keywords" content={metaKeywords} />
-            <link rel="shortcut icon" type="image/jpg" href={faviconImg}/>
+            {/*<link rel="shortcut icon" type="image/jpg" href={favicon}/>*/}
+            <link rel="icon" type="image/svg+xml" href={faviSvg} />
+            <link rel="alternate icon" href={favicon} />
+            <link rel="mask-icon" href={faviSvg} color="#ff8a01" />
         </Helmet>
     </div>
   );
