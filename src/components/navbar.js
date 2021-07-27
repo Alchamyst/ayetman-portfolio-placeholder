762a44e3ebb7a93698163b9760d67b994c73cf2f
Alchamyst <ashley@ashley-yetman.co.uk>
2021-07-27 16:59:09 +0100

    refactor navbar component and add styling.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import * as style from './navbar.module.css';
 
-const Navbar = ({ children }) => {
+const Navbar = () => {
   return (
     <div>
-        <nav>
-            <ul>
+        <nav class={style.navbar}>
+            <ul class={style.navbar_items}>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/contact">Contact</Link></li>
             <li><Link to="/about">About</Link></li>
             </ul>
         </nav>
-        {children}
     </div>
   );
 };
