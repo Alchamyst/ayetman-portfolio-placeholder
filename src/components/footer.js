3d4eaa8b7f2385980f6c7157d3bfaa6c4179f532
Alchamyst <ashley@ashley-yetman.co.uk>
2021-07-27 18:53:07 +0100

    refactor footer component. Add image imports for footer and header favicon.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import * as style from './footer.module.css';
+import linImg from "../images/linkedin-logo.png"
 
 const Footer = () => {
   return (
     <div class={style.footer}>
         <div>
-            <Link to="https://www.linkedin.com/in/ashley-yetman/">LinkedIn</Link>
+            <a href="https://www.linkedin.com/in/ashley-yetman/"><img src={linImg} alt="LinkedIn" width="50em" height="50em" /></a>
         </div>
         <div>
             &copy; 2021 Ashley Yetman. All Rights Reserved.
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
+import faviconImg from "../images/icon.png"
 
 const Header = (metaKeywords) => {
   return (
     <div>
         <Helmet>
             <meta name="keywords" content={metaKeywords} />
+            <link rel="shortcut icon" type="image/jpg" href={faviconImg}/>
         </Helmet>
     </div>
   );
