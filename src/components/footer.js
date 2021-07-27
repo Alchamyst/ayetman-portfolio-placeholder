648186970b4dc8b4118757e5e626c2d1d877fa91
Alchamyst <ashley@ashley-yetman.co.uk>
2021-07-27 22:39:07 +0100

    remove linkedin from footer. Refactor main css.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import * as style from './footer.module.css';
-import linImg from "../images/linkedin-logo.png"
 
 const Footer = () => {
   return (
     <div class={style.footer}>
-        <div>
-            <a href="https://www.linkedin.com/in/ashley-yetman/"><img src={linImg} alt="LinkedIn" width="50em" height="50em" /></a>
-        </div>
         <div>
             &copy; 2021 Ashley Yetman. All Rights Reserved.
         </div>
