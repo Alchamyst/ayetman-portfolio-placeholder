339a0e825e5fbf82084ae1038e3d790dd6b1822b
Alchamyst <ashley@ashley-yetman.co.uk>
2021-07-28 11:47:39 +0100

    Remove h1 tag from main component.

--- src/components/main.js
+++ src/components/main.js
@@ -5,7 +5,6 @@ const Main = ({ pageTitle, children }) => {
   return (
     <main className={style.main}>
         <title>{pageTitle}</title>
-        <h1>{pageTitle}</h1>
         {children}
     </main>
   );
