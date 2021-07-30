8d4dff27d97e3f6de8f038cb21715b72b37f3ad5
Alchamyst <ashley@ashley-yetman.co.uk>
2021-07-30 22:24:11 +0100

    add github img on homepage which links to Alchamyst's GitHub.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import profileImg from '../images/profile.gif';
+import gitHubImg from '../images/github-64px.png';
 
 const metaKeywords = [
   "ashley yetman", 
@@ -13,7 +14,8 @@ const IndexPage = () => {
       <Layout pageTitle="Home Page" metaKWs={metaKeywords}>
         <img src={profileImg} height="15%" width="15%"alt="Ashley Yetman"/>
         <p>Hey, I'm Ashley yetman.</p>
-        <p></p>
+        <br />
+        <p><a href="https://github.com/Alchamyst"><img src={gitHubImg} height="7.5%" width="7.5%" alt="Alchamyst GitHub"/></a></p>
       </Layout>
     </div>
   )
