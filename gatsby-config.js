76873d6f71667684b55b61148b167f8f0f6a392f
Alchamyst <ashley@ashley-yetman.co.uk>
2021-08-04 20:23:17 +0100

    Remove build command from deploy script + update config for gh-pages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     siteUrl: "https://www.ashley-yetman.co.uk",
     title: "Ashley Yetman Portfolio",
   },
-  pathPrefix: "/ayetman-portfolio",
   plugins: [
       "gatsby-plugin-gatsby-cloud", 
       "gatsby-plugin-react-helmet",
