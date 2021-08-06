9124ff100696ec7d63a26202db9198416b41d133
Alchamyst <ashley@ashley-yetman.co.uk>
2021-08-06 23:26:34 +0100

    Remove 'about' from navbar and turn on navbar.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,16 +4,16 @@ import * as style from './navbar.module.css';
 
 const Navbar = () => {
   return (
-    <div className={style.headerContainer}>    
-        <div className={style.headerItem}>
-         <nav className={style.navbar}>
-              <ul className={style.navbar_items}>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
-              <li><Link to="/about">About</Link></li>
-              </ul>
-          </nav>
-        </div>
+    <div className={style.headerContainer}>   
+      <div className={style.headerItem}>
+        <nav className={style.navbar}>
+          <ul className={style.navbar_items}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            {/* <li><Link to="/about">About</Link></li> */}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
