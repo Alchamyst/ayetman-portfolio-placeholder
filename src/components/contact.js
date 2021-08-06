71da26b517cdfe5b50d1190b8f1d2b822a47f029
Alchamyst <ashley@ashley-yetman.co.uk>
2021-08-07 00:10:47 +0100

    create new Contact component and relevant module.css

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as style from './contact.module.css';
+
+const Contact = () => {
+  return (
+    <div className={style.contact}>
+        <p>Contact form will go here!</p>
+    </div>
+  );
+};
+
+export default Contact;
