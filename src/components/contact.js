e8fd0671373c9744207e460942e8de90ad8e3e0e
Alchamyst <ashley@ashley-yetman.co.uk>
2021-08-09 22:08:11 +0100

    add 'require' attribute to form fields. Update styles.

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
         <h1 className={style.formHeading}>Contact Ashley</h1>
         <p className={style.formInfo}>Get in touch by filling out the form below.</p>
         <form className={style.contactForm} method="post" action="#">
-          <input className={style.formInput} type="text" name="name" id="name" aria-label="name" placeholder="Full Name"/>
-          <input className={style.formInput} type="email" name="email" id="email" aria-label="email" placeholder="Email Address" />
-          <textarea className={style.formTextArea} name="message" id="message" aria-label="message" placeholder="How can I help?" />
+          <input className={style.formInput} type="text" name="name" id="name" aria-label="name" placeholder="Full Name" required />
+          <input className={style.formInput} type="email" name="email" id="email" aria-label="email" placeholder="Email Address" required />
+          <textarea className={style.formTextArea} name="message" id="message" aria-label="message" placeholder="How can I help?" required />
           <button className={style.formSubmitBtn} type="submit">Send Message</button>
         </form>
     </div>
